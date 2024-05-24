16957920255c7f0461282b6516b04a5ff6a3c8cc
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-05-24 20:36:14 +0530

    Seting up for NextJs Mongo Auth Tutorial

--- model/CreateTodoSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-const CreateTodoSchema = z.object({
-  title: z.string().min(1).max(24),
-});
-
-export default CreateTodoSchema;
--- model/LoginFormSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-const LoginFormSchema = z.object({
-  emailAddress: z.string().email(),
-  password: z.string().min(8),
-});
-
-export default LoginFormSchema;
